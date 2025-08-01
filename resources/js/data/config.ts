3160c0bdde872fd670964a4f85203f4df04aa9ed
soyjson <clandyjoshua@hotmail.com>
2025-08-01 20:04:47 +0700

    v1.0

--- resources/js/data/config.ts
+++ resources/js/data/config.ts
@@ -47,11 +47,11 @@ export const VIDEO_SWITCH_DATE = FORM_DATE;
 
 export const VIDEO_BANNER =
     new Date() >= VIDEO_SWITCH_DATE
-        ? "https://gg6oykpzrfkxfwru.public.blob.vercel-storage.com/videos/mcb.mp4"
-        : "https://gg6oykpzrfkxfwru.public.blob.vercel-storage.com/videos/teaser-2025.mp4"; // ubah file video banner
+        ? "/videos/mcb.mp4"
+        : "/videos/teaser-2025.mp4"; // ubah file video banner
 
 export const HUTRI_LOGO =
-    "https://gg6oykpzrfkxfwru.public.blob.vercel-storage.com/images/logo/hutri/hut80ri.svg"; // ubah file logo hut ri
+    "https://cdn.discordapp.com/attachments/1400811580383297546/1400822304983748668/hut80ri.png?ex=688e08ef&is=688cb76f&hm=57440615ae1daf7f5ea18baa985217f6c2592dfc286cb03bd64f1bf38e03543b&"; // ubah file logo hut ri
 //
 
 // SETUP TIMELINE ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -196,32 +196,32 @@ export const creditsConfig: CreditInfo[] = [
         plots: [
             {
                 name: "MinecraftID",
-                image: "https://gg6oykpzrfkxfwru.public.blob.vercel-storage.com/images/logo/komunitas/2025/minecraftid.svg",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400822676167327775/minecraftid.png?ex=688e0948&is=688cb7c8&hm=b3bff40f056ea2bcfcb27c92d0c5d3fd3a9755b3c993cd3a7b7d5babaf1ba27b&",
                 url: "https://www.minecraft-id.net/",
             },
             {
                 name: "Minecraft Sejati",
-                image: "https://gg6oykpzrfkxfwru.public.blob.vercel-storage.com/images/logo/komunitas/2025/mcsejati.svg",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400822733134237828/mcsejati.png?ex=688e0955&is=688cb7d5&hm=36534420bd9fc73eb3a42c7acd0b1b1ffd66c3559f5e180ed5337758fc02e524&",
                 url: "https://s.id/mcsejatigroups",
             },
             {
                 name: "MineInd Community",
-                image: "https://gg6oykpzrfkxfwru.public.blob.vercel-storage.com/images/logo/komunitas/2025/mineind.svg",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400822808443097148/mineind.png?ex=688e0967&is=688cb7e7&hm=b42ea816e2baf709c3e9189a66a3baad10aad9d065cb2bacfa06d6d0a46c3a63&",
                 url: "https://dsc.gg/mineind",
             },
             {
                 name: "Chronoscraft",
-                image: "https://gg6oykpzrfkxfwru.public.blob.vercel-storage.com/images/logo/komunitas/2025/chronoscraft.svg",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400822918530990231/chronoscraft.png?ex=688e0981&is=688cb801&hm=66d0eb65e29489571a78f6ac2ba4fbc4b9f00cda4da6ec845981e23c38dcbfb4&",
                 url: "https://chat.whatsapp.com/HLHWgwpmDAnCnJmIwWIq4a?mode=r_t",
             },
             {
                 name: "Sahabat Minecrafter",
-                image: "https://gg6oykpzrfkxfwru.public.blob.vercel-storage.com/images/logo/komunitas/2025/sahabatmc.svg",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400822979448934514/sahabatmc.png?ex=688e0990&is=688cb810&hm=ffd5b9716518198844244a3fa9044efac6813a0494b24b082c3a20998f25d046&",
                 url: "https://sahabatmcid.my.id",
             },
             {
                 name: "Motion Minecraft",
-                image: "https://gg6oykpzrfkxfwru.public.blob.vercel-storage.com/images/logo/komunitas/2025/motionmc.svg",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400823049498132570/motionmc.png?ex=688e09a1&is=688cb821&hm=bf1e679d951abc9899916e52ee3d0f6868c992fea7c9be5b7ce76208751be392&",
                 url: "https://discord.gg/motionime",
             },
             // {
@@ -251,7 +251,7 @@ export const creditsConfig: CreditInfo[] = [
         plots: [
             {
                 name: "YueMi Development",
-                image: "https://gg6oykpzrfkxfwru.public.blob.vercel-storage.com/images/logo/mitra/2025/yuemi.svg",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400823247104249897/yuemi.png?ex=688e09d0&is=688cb850&hm=90da5b9898d2a51849f5fd221a8d90e019dca7db35a6876bb92cff06e7065340&",
                 url: "https://dsc.gg/yuemi",
             },
             // {
@@ -285,25 +285,25 @@ export const creditsConfig: CreditInfo[] = [
         title: "Mitra Media",
         plots: [
             {
-                name: "",
-                image: "",
-                url: "",
+                name: "Warta Minecraft",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400823324451409970/wartamc.png?ex=688e09e2&is=688cb862&hm=2f929a1a1e13f8395014f2dffb6e3c15aa633780934ec09af97395c05bb81ee6&",
+                url: "https://www.instagram.com/wartamc",
             },
-            {
-                name: "",
-                image: "",
-                url: "",
-            },
-            {
-                name: "",
-                image: "",
-                url: "",
-            },
-            {
-                name: "",
-                image: "",
-                url: "",
-            },
+            // {
+            //     name: "",
+            //     image: "",
+            //     url: "",
+            // },
+            // {
+            //     name: "",
+            //     image: "",
+            //     url: "",
+            // },
+            // {
+            //     name: "",
+            //     image: "",
+            //     url: "",
+            // },
         ],
     },
 ];

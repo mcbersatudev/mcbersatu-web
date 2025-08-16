2576f40ecd4b5aa805da4a05a157a4a70cf50092
soyjson <clandyjoshua@hotmail.com>
2025-08-17 04:00:32 +0700

    v1.0

--- resources/js/data/config.ts
+++ resources/js/data/config.ts
@@ -26,13 +26,14 @@ export const CALENDAR_MSG = "";
 
 // Form
 export const FORM_URL = "https://forms.gle/J6bZGmpmKxsqLeSU8"; // link gform pendaftaran peserta
-export const FORM_MSG = "Pendaftaran peserta dibuka hingga 17 Agustus 2025";
+export const FORM_MSG =
+    "Pendaftaran peserta diperpanjang hingga 23 Agustus 2025";
 export const FORM_DATE = new Date("2025-08-01T08:00:00"); // tanggal form dibuka
 
 // Livestream
 export const YT_URL = "https://www.youtube.com/@MineindCommunity"; // link channel yt tuan rumah
 export const YT_MSG = "Saksikan siaran langsung keseruan #MCB2025 sekarang!";
-export const YT_DATE = new Date("2025-08-24T12:30:00");
+export const YT_DATE = new Date("2025-08-31T12:30:00");
 
 // Alert
 export const ALERT_MSG = "Minecraft Bersatu Vol. 3 - 2025 resmi tamat";
@@ -47,11 +48,11 @@ export const VIDEO_SWITCH_DATE = FORM_DATE;
 
 export const VIDEO_BANNER =
     new Date() >= VIDEO_SWITCH_DATE
-        ? "https://cdn.discordapp.com/attachments/1400811660540772373/1400979976001425448/mcb.mp4?ex=688e9bc7&is=688d4a47&hm=d3a89509753d4d530d4736d565b581c0e2e20dc0bb590871c2721fb5ace3cbfa&"
-        : "https://cdn.discordapp.com/attachments/1400811660540772373/1400980080427270358/teaser-2025.mp4?ex=688e9be0&is=688d4a60&hm=a41e74647271695ddf1d5eee7f08606f44aa607eb52ae5040b366aedb725190f&"; // ubah file video banner
+        ? "https://cdn.discordapp.com/attachments/1400811660540772373/1400979976001425448/mcb.mp4?ex=68a1b987&is=68a06807&hm=82a401455b76522c26adf738078dd75b8396fa9d9af4107610f6a3f05e5ce484&"
+        : "https://cdn.discordapp.com/attachments/1400811660540772373/1400980080427270358/teaser-2025.mp4?ex=68a1b9a0&is=68a06820&hm=b28b94ef8830fa0af7bfd992b5307e92ac3a9f708b382c4c33d8983008be1870&"; // ubah file video banner
 
 export const HUTRI_LOGO =
-    "https://cdn.discordapp.com/attachments/1400811580383297546/1400822304983748668/hut80ri.png?ex=688e08ef&is=688cb76f&hm=57440615ae1daf7f5ea18baa985217f6c2592dfc286cb03bd64f1bf38e03543b&"; // ubah file logo hut ri
+    "https://cdn.discordapp.com/attachments/1400811580383297546/1400822304983748668/hut80ri.png?ex=68a1cf6f&is=68a07def&hm=902a96d949aecc563018a94631d59786799be578b9a884b5c9a09a48f85ec203"; // ubah file logo hut ri
 //
 
 // SETUP TIMELINE ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -77,10 +78,10 @@ export const tabsConfig: TabInfo[] = [
         date: new Date("2025-08-01"),
         slots: [
             {
-                time: "08.00 WIB - 17/08",
+                time: "08.00 WIB - 23/08",
                 title: "Pendaftaran peserta",
                 description:
-                    "Dibuka pendaftaran peserta hingga tanggal 17 Agustus 2025.",
+                    "Dibuka pendaftaran peserta hingga tanggal 23 Agustus 2025.",
             },
         ],
     },
@@ -196,32 +197,32 @@ export const creditsConfig: CreditInfo[] = [
         plots: [
             {
                 name: "MinecraftID",
-                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400822676167327775/minecraftid.png?ex=688e0948&is=688cb7c8&hm=b3bff40f056ea2bcfcb27c92d0c5d3fd3a9755b3c993cd3a7b7d5babaf1ba27b&",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400822676167327775/minecraftid.png?ex=68a1cfc8&is=68a07e48&hm=a51c314e66c7999fc826c5a79d2a1169d137a3dc83cd509fb9fd0a2dc200c159",
                 url: "https://www.minecraft-id.net/",
             },
             {
                 name: "Minecraft Sejati",
-                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400822733134237828/mcsejati.png?ex=688e0955&is=688cb7d5&hm=36534420bd9fc73eb3a42c7acd0b1b1ffd66c3559f5e180ed5337758fc02e524&",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400822733134237828/mcsejati.png?ex=68a1cfd5&is=68a07e55&hm=3ab94b7be323cfda259ae6fa3aa4c72f38274121a5e146beab09a3bf92ea156c",
                 url: "https://s.id/mcsejatigroups",
             },
             {
                 name: "MineInd Community",
-                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400822808443097148/mineind.png?ex=688e0967&is=688cb7e7&hm=b42ea816e2baf709c3e9189a66a3baad10aad9d065cb2bacfa06d6d0a46c3a63&",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400822808443097148/mineind.png?ex=68a1cfe7&is=68a07e67&hm=39847d2e7ff1e5dbe2124474cedacbeab54e2d2c47f164332986798332372a04",
                 url: "https://dsc.gg/mineind",
             },
             {
                 name: "Chronoscraft",
-                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400830453425897595/chronoscraft.png?ex=688e1086&is=688cbf06&hm=73bc81a03015c57278044c88580b9c3c3df80ff8acf0846bdf628bfa4e855216&",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400830453425897595/chronoscraft.png?ex=68a1d706&is=68a08586&hm=1695771d7609bb8ea646a31291c8ca73252c0fc71a0d57137e594adc3ccf30c3",
                 url: "https://chat.whatsapp.com/HLHWgwpmDAnCnJmIwWIq4a?mode=r_t",
             },
             {
                 name: "Sahabat Minecrafter",
-                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400822979448934514/sahabatmc.png?ex=688e0990&is=688cb810&hm=ffd5b9716518198844244a3fa9044efac6813a0494b24b082c3a20998f25d046&",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400822979448934514/sahabatmc.png?ex=68a1d010&is=68a07e90&hm=9c55a75858105a29d6faa0078465aa4b86fdc33fea1d70bdb2e63073a2963752",
                 url: "https://sahabatmcid.my.id",
             },
             {
                 name: "Motion Minecraft",
-                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400823049498132570/motionmc.png?ex=688e09a1&is=688cb821&hm=bf1e679d951abc9899916e52ee3d0f6868c992fea7c9be5b7ce76208751be392&",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400823049498132570/motionmc.png?ex=68a1d021&is=68a07ea1&hm=54c6abbb5829ebf7004a38959c1ec71b0dd6f4d07852977f0bccc6a9a8c22dc5",
                 url: "https://discord.gg/motionime",
             },
             // {
@@ -251,14 +252,14 @@ export const creditsConfig: CreditInfo[] = [
         plots: [
             {
                 name: "YueMi Development",
-                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400823247104249897/yuemi.png?ex=688e09d0&is=688cb850&hm=90da5b9898d2a51849f5fd221a8d90e019dca7db35a6876bb92cff06e7065340&",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400823247104249897/yuemi.png?ex=68a1d050&is=68a07ed0&hm=ed13c28522e6912bbcf47119e3d8c31915e588b0bbf3b15629465a4747042eda",
                 url: "https://dsc.gg/yuemi",
             },
-            // {
-            //     name: "",
-            //     image: "",
-            //     url: "",
-            // },
+            {
+                name: "Raznar",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1406092531464147035/raznar.png?ex=68a1ddf7&is=68a08c77&hm=2c9f684b0a303deb05363f122b787be2a8198e72015dca2319656af4b019f278",
+                url: "https://raznar.id/",
+            },
         ],
     },
     {
@@ -286,7 +287,7 @@ export const creditsConfig: CreditInfo[] = [
         plots: [
             {
                 name: "Warta Minecraft",
-                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400823324451409970/wartamc.png?ex=688e09e2&is=688cb862&hm=2f929a1a1e13f8395014f2dffb6e3c15aa633780934ec09af97395c05bb81ee6&",
+                image: "https://cdn.discordapp.com/attachments/1400811580383297546/1400823324451409970/wartamc.png?ex=68a1d062&is=68a07ee2&hm=e83fd7b7357ad8fc072932f3f7f56941b26a587acd6a3d88f22e1568bd864527",
                 url: "https://www.instagram.com/wartamc",
             },
             // {
